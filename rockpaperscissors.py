dac9aeb64ae0c23f609772ab8e79e1156c32c68e
beetlesoup <sairafi.b@gmail.com>
2021-07-11 15:22:15 +0300

    Create the play_verifier() function

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,6 +1,23 @@
 
 from random import randint
 
+def play_verifier(play):
+    """this function is so the player can enter more than one possible
+    value, and the programmer can act like they only recieve the
+    cleanest of data entries. Would you call this an elegant solution?
+    For a noob, at least."""
+    
+    if player.lower()[0] == "r" or "1":
+        player = "Rock"
+    elif player.lower()[0] == "p" or "2":
+        player = "Paper"
+    elif player.lower()[0] == "s" or "3":
+        player = "Scissors"
+
+    return player
+
+
+
 hand = ["Rock", "Paper", "Scissors"]
 
 computer = hand[randint(0,2)]
@@ -8,8 +25,16 @@ computer = hand[randint(0,2)]
 player = False
 
 while player == False:
-    player = input("Rock, Paper, Scissors?")
+    player = input("Rock, Paper, Scissors?").lower()
+
+    if player in names:
 
 
-player = False
-computer = hand[randint(0,2)]
+        if player == computer:
+            print("Computer played {}. Curious. Why did you choose the same thing?".computer)
+        elif player
+
+    else player = input("Bad entry, bucko. R, P, S?")
+
+    player = False
+    computer = hand[randint(0,2)]
